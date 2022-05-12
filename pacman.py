25e811023172edff945b9709a0fb86518d44b314
ALAIN <A01620758@tec.mx>
2022-05-12 18:36:22 -0500

    El código del juego Pacman aparecerá correcto según estándares de flake 8

--- pacman.py
+++ pacman.py
@@ -10,7 +10,9 @@ Exercises
 """
 
 from random import choice
-from turtle import *
+from turtle import Turtle, bgcolor, clear, up, goto
+from turtle import dot, update, ontimer, setup, hideturtle
+from turtle import tracer, listen, onkey, done
 
 from freegames import floor, vector
 
